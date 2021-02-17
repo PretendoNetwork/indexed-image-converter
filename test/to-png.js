f413e562396ac78cb71b6ab8dc8164bc077dd839
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-02-16 19:38:13 -0500

    height is not width

--- test/to-png.js
+++ test/to-png.js
@@ -3,8 +3,8 @@ const { toImage } = require('..');
 
 const data = fs.readFileSync('./image.data'); // read the indexed image data
 const width = 854; // WiiU gamepad width
-const height = 400; // WiiU gamepad width
-const type = 'png'; // image type to convert to
+const height = 400; // WiiU gamepad height
+const type = 'png'; // image type to convert to (supports png, gif, tif, bmp, jpg)
 
 const png = toImage(data, width, height, type); // convert indexed image to PNG
 
